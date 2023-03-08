079840cabf260776fa9b9fcfb804f6844e35f1ce
Aljith KJ <kwdeathlok@gmail.com>
2023-03-08 21:25:07 +0530

    HangmanWord component added

--- src/Components/HangmanWord.tsx
+++ src/Components/HangmanWord.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
 const HangmanWord = () => {
+    const word = 'test';
+    const guessedLetters = ['t', 'e', 'g'];
   return (
-    <div>
-      
+    <div style={{ display: 'flex', gap: '.25em', fontSize: '6rem', fontWeight: 'bold', 
+        textTransform: 'uppercase', fontFamily: 'monospace'
+    }}>
+      { word.split("").map((letter, ind) => (
+            <span key={ ind } style={{
+                borderBottom: '.1em solid black'
+            }}>
+                <span style={{
+                    visibility: guessedLetters.includes(letter) ? 'visible': 'hidden'
+                }}>
+                    { letter }
+                </span>
+            </span>
+        ))}
     </div>
   )
 }
